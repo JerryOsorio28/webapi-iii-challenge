76f5eed5299705f3032d27c0e074c4bbedd8f10b
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-09-04 17:33:09 -0400

    Got logger function to log properly

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code away!
+//Pulls express dependency
+const express = require('express');
+
+//Run our server under the express depencency
+const server = express();
+
+//Teaches express to parse JSON
+server.use(express.json()) 
+
+//Assign port that our server will be listening for traffic
+const port = 8000;
+
+//importing Database
+const userDatabase = ('./users/userDb.js')
+const postsDatabase = ('./posts/postDb.js')
+
+//custom middleware
+function logger(req, res, next) {
+    console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get('Origin')}`
+    )
+    next();
+  };
+
+server.get('/', logger, (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`)
+  });
+
+
+
+//Log in which port our server is listening for traffic
+server.listen(port , () => {
+    console.log(`server listening on port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-const express = 'express';
-
-const server = express();
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
-
 //custom middleware
-
 function logger(req, res, next) {
-
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get('Origin')}`
+  )
+  next();
 };
-
-module.exports = server;
